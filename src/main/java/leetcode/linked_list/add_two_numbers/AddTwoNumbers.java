268be408ab0c6b45f32ebc5ad2ee707b3c8178cb
OnlyLoveTianYa <763149560@qq.com>
2018-05-29 17:53:40 +0800

    init

--- src/main/java/leetcode/linked_list/add_two_numbers/AddTwoNumbers.java
+++ src/main/java/leetcode/linked_list/add_two_numbers/AddTwoNumbers.java
@@ -1,22 +1,20 @@
 package leetcode.linked_list.add_two_numbers;
 
 /**
- * <p>文件名称: </p>
- * <p>文件描述: </p>
- * <p>版权所有: 版权所有(C)2017</p>
- * <p>公 司: 深圳竹云科技有限公司</p>
- * <p>内容摘要: </p>
- * <p>其他说明: 无</p>
- * <p>完成日期: 2018/5/23</p>
+ * 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
  *
  * @author : OnlyLoveTianYa
  */
 public class AddTwoNumbers {
     public static void main(String[] args) {
-
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         ListNode l1 = new ListNode(5);
-//        l1.next = new ListNode(8);
         ListNode l2 = new ListNode(5);
         addTwoNumbers.addTwoNumbers(l1, l2);
     }
